c7df69c86a01bdd98be94f84de40d034a21fb279
lilzinn <80847791+lilzinn@users.noreply.github.com>
2021-03-27 10:54:49 -0300

    Update numowner.js

--- numowner.js
+++ numowner.js
@@ -1,10 +1,10 @@
-const ownerNumber = ["557187645787@s.whatsapp.net"]
-const OriginalOwner = '557187645787'
+const ownerNumber = ["557388843631@s.whatsapp.net"]
+const OriginalOwner = '557388843631'
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
             + 'FN:Meu criador^~^\n' 
             + 'ORG:Ian;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=557388843631:+55 73 8884-3631\n' 
             + 'END:VCARD'
 
 exports.vcard = vcard
