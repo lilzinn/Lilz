26f4525a12bb3b56a9f74bd78f44f5d2ddda18e8
lilzinn <80847791+lilzinn@users.noreply.github.com>
2021-03-30 11:36:47 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,140 @@
+const ownerNumber = ["557388843631@s.whatsapp.net"] 
+// mude somente o numero e não tire o @s.whatsapp.net
+
+const OriginalOwner = '557388843631' 
+// mude para seu numero
+
+const cr = 'BRIZAS BOT\n' 
+// texto do simbolo de verificado
+
+const prefix = '-' 
+// prefixo
+
+
+// MENSAGENS DE BAN
+
+const banmsgtype = '*ᴠᴀᴄɪʟᴏ ᴀǫᴜɪ ᴇ sᴏ ᴜᴍ, ᴛᴄʜᴀᴜ ᴏᴛᴀʀᴏ🙄🤙*'
+// mensagem de ban no anti tipos de mensagem
+
+const adminmsgtype = '*ᴛᴜ ᴇ ᴀᴅᴍ ᴇɴᴛ ɴ ᴠᴏᴜ ᴛᴇ ʙᴀɴɪʀ ᴘᴏʀ ᴍᴀɴᴅᴀʀ ᴜᴍ ᴛɪᴘᴏ ᴅᴇ ᴍᴇɴsᴀɢᴇᴍ ᴘʀᴏɪʙɪᴅᴀ*'
+// mensagem de quando adm manda tipos de mensagens proibidas
+
+const banmsgporn = 'ʜᴍᴍᴍ ᴘᴏʀɴᴏᴢɪɴʜᴏ ᴄᴏᴍ ᴀɴᴛɪᴘᴏʀɴ ᴀᴛɪᴠᴀᴅᴏ, ᴊᴀ sᴀʙᴇ ɴᴇ...'
+// mensagem de ban no antiporn
+
+const adminmsgporn = '*ᴛᴜ ᴇ ᴀᴅᴍ ᴇɴᴛ ɴ ᴠᴏᴜ ᴛᴇ ʙᴀɴɪʀ ᴘᴏʀ ᴍᴀɴᴅᴀʀ ᴘᴏʀɴ*'
+// mensagem de quando adm manda porn com antiporn ativado
+
+const banmsglink = 'ᴠᴀᴄɪʟᴏ ᴀǫᴜɪ ᴇ sᴏ ᴜᴍ, ᴛᴄʜᴀᴜ ᴏᴛᴀʀᴏ🙄🤙'
+// mensagem de ban no antilink
+
+const adminmsglink = 'ɪssᴏ ᴇ ᴜᴍ ʟɪɴᴋ ᴀᴍɪɢᴏ... ᴀʜ ᴇ ᴛᴜ ᴇ ᴀᴅᴍ ᴇɴᴛ ᴘᴏᴅᴇ ᴋᴋᴋᴋ 🙃'
+// mensagem de quando adm manda link
+
+
+// MENSAGENS DE BOM DIA BOA TARDE E BOA NOITE
+
+const linkimgday = 'https://i.imgur.com/RR18JiI.jpg'
+const textmsgday = 'ᴏʜᴀʏᴏ ɢᴏᴢᴀɪᴍᴀsᴜ, ᴏɴɪɪ-ᴄʜᴀɴ 👉👈'
+// link e mensagem de bom dia
+
+const linkimgeve = 'https://i.ibb.co/bLXgcBW/lp-tarde-anime-by-hitsukinyan-db7fpqr-fullview.jpg'
+const textmsgeve = `ᴋᴏɴ'ɴɪᴄʜɪᴡᴀ ᴏɴɪɪ-ᴄʜᴀɴ, ᴀᴄᴇɪᴛᴇ ᴇssᴇ ᴄᴀғᴇ  ☕`
+// link e mensagem de boa tarde
+
+const linkimgnig = 'https://i.ibb.co/VJCd34F/EKvo372-X0-AEd-Hz.jpg'
+const textmsgnig = 'ᴋᴏɴʙᴀɴᴡᴀ ᴏɴɪɪ-ᴄʜᴀɴ, ᴇsᴘᴇʀᴏ ǫᴜᴇ ᴛᴇɴʜᴀ ᴛɪᴅᴏ ᴜᴍ ʙᴏᴍ ᴅɪᴀ ☺️'
+// link e mensagem de boa noite 
+
+// MENSAGENS DE ERRO
+
+const msgerr = 'ᴅᴇᴜ ᴇʀʀᴏ, ᴛᴇɴᴛᴇ ɴᴏᴠᴀᴍᴇɴᴛᴇ :/'
+// mensagem de erro
+
+// MENSAGENS DE AUTOREPLY
+
+const botlindo = 'ʙᴀᴋᴀᴀᴀᴀ 😣😣'
+const linkbotlindo = 'https://photos1.iorbix.com/00/00/00/00/02/72/43/64/C--6tZwaASQH-b.jpg'
+const botfeio = 'ᴛᴜ ᴘᴀʀᴇᴄᴇ ᴜᴍ sɪʀɢᴜᴇɪᴊᴏ, ᴜᴍ ᴄᴀʀᴀɴɢᴜᴇᴊᴏ ᴀᴍᴀssᴀᴅᴏ 😡'
+const cadebot = 'ᴍᴇ ᴄʜᴀᴍᴏᴜ ᴏɴɪɪ-ᴄʜᴀɴ 👉👈?'
+const botfdp = 'ᴛᴇ ғᴏᴅᴇʀ ʀᴀᴘᴀᴢ, ᴛᴇ ᴅᴇɪᴛᴀʀ ɴᴀ ᴘᴏʀʀᴀᴅᴀ ǫᴜᴀɴᴅᴏ ᴛɪᴠᴇʀ ᴅᴏʀᴍɪɴᴅᴏ'
+const botgostoso = 'ᴀʀɪɢᴀᴛᴏ ɢᴏ sᴀɪ ᴍᴀsᴜ 😳👉👈'
+const botfofo = 'ᴀʀɪɢᴀᴛᴏ ɢᴏ sᴀɪ ᴍᴀsᴜ 😳👉👈'
+const botbaianor = 'ᴄᴏɴᴠɪᴅᴇɪ sᴜᴀ ᴘʟᴀᴄᴀ-ᴍᴀᴇ ᴘʀᴀ ᴍɪɴʜᴀ ʀᴇᴅᴇ sᴇᴜ ᴄᴏʀɴᴏ'
+const botcorno = 'ᴠᴀɪ ᴛᴇ ғᴏᴅᴇʀ, ᴊᴏɢᴀᴅᴏʀ ᴅᴇ ғғ, ᴄᴏᴍᴘʀᴀᴅᴏʀ ᴅᴇ ᴘᴀᴄᴋ ᴅᴏ ᴘᴇᴢɪɴʜᴏ'
+const botputa = 'sᴇɴʜᴏʀᴀ sᴜᴀ ᴍᴀᴇ 😡'
+const botgay = 'ғᴀʟᴏᴜ ᴏ ᴍᴀɴᴊᴀ ʀᴏʟᴀ 😡'
+const botviado = 'ғᴀʟᴏᴜ ᴏ ᴍᴀɴᴊᴀ ʀᴏʟᴀ 😡'
+const numbotfeio = 'sᴇɴʜᴏʀᴀ sᴜᴀ ᴍᴀᴇ 😡'
+// KEYS
+
+const apiTobz = 'key'
+// pra pegar a key do Tobz va até o numero 
+//wa.me/447418349954 se registre no bot
+// e depois escreva #generateapikey que o 
+//bot do tobz vai gerar sua key
+
+// MENSAGENS DE EXCLUSIVIDADE
+let mess = {
+    wait: '*⏳ ᴇsᴘᴇʀᴇ ᴛᴏᴍᴇ  ᴜᴍ ᴄᴀғᴇ ☕ ᴇɴǫᴜᴀɴᴛᴏ ᴠᴏᴜ ᴛᴇ ᴇɴᴄᴀʀᴄᴀʀ ᴅᴇ ᴘᴇ...*',
+    success: '*sᴜᴄᴇssᴏ...*',
+    error: {
+        stick: '❌ ғᴀʟʜᴀ, ᴏᴄᴏʀʀᴇᴜ ᴜᴍ ᴇʀʀᴏ ᴀᴏ ᴄᴏɴᴠᴇʀᴛᴇʀ ᴀ ɪᴍᴀɢᴇᴍ ᴇᴍ ᴜᴍ ᴀᴅᴇsɪᴠᴏ ❌',
+        Iv: '❌ ʟɪɴᴋ ɪɴᴠᴀʟɪᴅᴏ ❌'
+    },
+    only: {
+        group: '❌ ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ sᴏ ᴘᴏᴅᴇ sᴇʀ ᴜsᴀᴅᴏ ᴇᴍ ɢʀᴜᴘᴏs! ❌',
+        ownerG: '❌ ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ sᴏ ᴘᴏᴅᴇ sᴇʀ ᴜsᴀᴅᴏ ᴘᴇʟᴏ ɢʀᴜᴘᴏ ᴘʀᴏᴘʀɪᴇᴛᴀʀɪᴏ! ❌',
+        ownerB: '❌ ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ sᴏ ᴘᴏᴅᴇ sᴇʀ ᴜsᴀᴅᴏ ᴘᴇʟᴏ ɴᴜᴍᴇʀᴏ ᴘʀᴏᴘʀɪᴇᴛᴀʀɪᴏ! ❌',
+        admin: '❌ sɪʟᴇɴᴄɪᴏ ᴍᴇᴍʙʀᴏ ᴄᴏᴍᴜᴍ ᴠᴄ ɴ ᴛᴇᴍ ᴍᴏʀᴀʟ ᴘʀᴀ ᴜsᴀʀ ᴇssᴇ ᴄᴏᴍᴀɴᴅᴏ ❌',
+        Badmin: '❌ ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ sᴏ ᴘᴏᴅᴇ sᴇʀ ᴜsᴀᴅᴏ ǫᴜᴀɴᴅᴏ ᴏ ʙᴏᴛ sᴇ ᴛᴏʀɴᴀ ᴀᴅᴍɪɴɪsᴛʀᴀᴅᴏʀ! ❌'
+    }
+}
+
+// CONTATO DO CRIADOR
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:Meu criador^~^\n' 
++ 'ORG:Ian;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=557388843631:+55 73 8884-3631\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+
+// Nem ouse mexer aqui se n quiser que de erro
+
+exports.linkbotlindo = linkbotlindo
+exports.botlindo = botlindo
+exports.botfeio = botfeio
+exports.cadebot = cadebot
+exports.botfdp = botfdp
+exports.botgostoso = botgostoso
+exports.botfofo = botfofo
+exports.botbaianor = botbaianor
+exports.botcorno = botcorno
+exports.botputa = botputa
+exports.botgay = botgay
+exports.botviado = botviado
+exports.numbotfeio = numbotfeio
+exports.linkimgday = linkimgday
+exports.textmsgday = textmsgday
+exports.textmsgnig = textmsgnig
+exports.linkimgnig = linkimgnig
+exports.textmsgeve = textmsgeve
+exports.linkimgeve = linkimgeve
+exports.adminmsgtype = adminmsgtype
+exports.adminmsgporn = adminmsgporn
+exports.adminmsglink = adminmsglink
+exports.banmsglink = banmsglink
+exports.banmsgporn = banmsgporn
+exports.banmsgtype = banmsgtype
+exports.msgerr = msgerr
+exports.mess = mess
+exports.apiTobz = apiTobz
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
