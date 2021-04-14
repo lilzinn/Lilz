e67f9014715f09874f630bb56945f0bb2bea063a
lilzinn <80847791+lilzinn@users.noreply.github.com>
2021-04-14 19:57:14 -0300

    Delete welcomtext.js

--- welcomtext.js
+++
@@ -1,9 +0,0 @@
-const welcome = (number, groupname) => {
-    return `ᴏʟᴀ @${number}. sᴇᴊᴀ ʙᴇᴍ-ᴠɪɴᴅᴏ ᴀᴏ ɢʀᴜᴘᴏ ${groupname}`
-}
-exports.welcome = welcome
-
-const bye = (number) => {
-    return `ᴀᴅᴇᴜs @${number}. ᴊᴀ ᴠᴀɪ ᴛᴀʀᴅᴇ 🙄🤙`
-}
-exports.bye = bye
